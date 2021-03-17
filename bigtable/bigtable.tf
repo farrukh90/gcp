6b74aaea7c6071fe2894d2508905155120f0c160
farrukh90 <farrukhsadykov@gmail.com>
2021-03-17 04:12:34 +0000

    added more info

--- bigtable/bigtable.tf
+++ bigtable/bigtable.tf
@@ -1,6 +1,6 @@
 resource "google_bigtable_instance" "production-instance" {
-  name = var.config["name"]
-  deletion_protection = false
+  name                = var.config["name"]
+  deletion_protection = var.config["deletion_protection"]
   cluster {
     cluster_id   = var.config["cluster_id"]
     num_nodes    = var.config["num_nodes"]
