7e913d156db24f3054a3ddb9e264364a34807e25
farrukh90 <farrukhsadykov@gmail.com>
2021-03-17 04:25:30 +0000

    google_firestore_document.mydoc

--- firestore/output.tf
+++ firestore/output.tf
@@ -1,6 +1,6 @@
 output "config" {
   value = [
-    google_firestore_index.index.id,
-    google_firestore_index.index.name
+    google_firestore_document.mydoc.id,
+    google_firestore_document.mydoc.name
   ]
 }
