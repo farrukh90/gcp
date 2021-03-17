be91b96e42a0a226a306f33e49f44eec670949b4
farrukh90 <farrukhsadykov@gmail.com>
2021-03-17 02:39:38 +0000

    added         google_sql_database_instance.instance.public_ip_address

--- cloudsql/output.tf
+++ cloudsql/output.tf
@@ -3,6 +3,8 @@ output config {
     google_sql_database_instance.instance.name,
     google_sql_database_instance.instance.connection_name,
     google_sql_database_instance.instance.first_ip_address,
-        google_sql_database_instance.instance.public_ip_address
+    google_sql_database_instance.instance.public_ip_address,
+    google_sql_database_instance.instance.self_link
+
   ]
 }
