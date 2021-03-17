7d3f4d7a4ddb3fd9d2ebf8789bf724a2b671126c
farrukh90 <farrukhsadykov@gmail.com>
2021-03-17 02:30:23 +0000

    added MYSQL_5_6, MYSQL_5_7, MYSQL_8_0, POSTGRES_9_6,POSTGRES_10, POSTGRES_11, POSTGRES_12, POSTGRES_13, SQLSERVER_2017_STANDARD, SQLSERVER_2017_ENTERPRISE, SQLSERVER_2017_EXPRESS, SQLSERVER_2017_WEB

--- cloudsql/variables.tf
+++ cloudsql/variables.tf
@@ -6,6 +6,6 @@ variable "config" {
   		region = "us-central1"
 		tier = "db-f1-micro"
 		deletion_protection  = "true"
-		database_version = "MYSQL_5_6"
+		database_version = "MYSQL_5_6"  #MYSQL_5_6, MYSQL_5_7, MYSQL_8_0, POSTGRES_9_6,POSTGRES_10, POSTGRES_11, POSTGRES_12, POSTGRES_13, SQLSERVER_2017_STANDARD, SQLSERVER_2017_ENTERPRISE, SQLSERVER_2017_EXPRESS, SQLSERVER_2017_WEB
 	}
 }
