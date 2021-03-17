dab0e5b512c9526b8d8e48916f58d62bc5040730
farrukh90 <farrukhsadykov@gmail.com>
2021-03-17 04:45:27 +0000

    removed users

--- firestore/variables.tf
+++ firestore/variables.tf
@@ -4,11 +4,5 @@ variable "config" {
     project      = "cache-307814"
     region       = "us-central1"
     zone         = "us-central1-c"
-    # name         = "tf-instance"
-    # cluster_id   = "tf-instance-cluster"
-    # num_nodes    = 1
-    # storage_type = "HDD"
-    # my-label     = "prod-label"
-    # deletion_protection = false
   }
 }
