15211575c2d31cb1daf029124f7051e626a042c3
farrukh90 <farrukhsadykov@gmail.com>
2021-03-17 04:31:48 +0000

    added values

--- firestore/firestore.tf
+++ firestore/firestore.tf
@@ -1,6 +1,6 @@
 resource "google_firestore_document" "mydoc" {
   project     = var.config["project"]
-  collection  = "somenewcollection"
-  document_id = "my-doc"
-  fields      = "{\"something\":{\"mapValue\":{\"fields\":{\"akey\":{\"stringValue\":\"avalue\"}}}}}"
+  collection  = "list"
+  document_id = "users"
+  fields      = "{\"something\":{\"mapValue\":{\"fields\":{\"firstname\":{\"stringValue\":\"farrukh\"}}}}}"
 }
