f84cb9a2da6ae490d31b2254656c481abbf7ba1e
farrukh90 <farrukhsadykov@gmail.com>
2021-03-17 04:33:38 +0000

    added resource "google_firestore_index" "my-index" {   project = "my-project-name"
      collection = "chatrooms"
    
      fields {
        field_path = "name"
        order      = "ASCENDING"
      }
    
      fields {
        field_path = "description"
        order      = "DESCENDING"
      }
    
      fields {
        field_path = "__name__"
        order      = "DESCENDING"
      }
    }

--- firestore/firestore.tf
+++ firestore/firestore.tf
@@ -4,3 +4,24 @@ resource "google_firestore_document" "mydoc" {
   document_id = "users"
   fields      = "{\"something\":{\"mapValue\":{\"fields\":{\"firstname\":{\"stringValue\":\"farrukh\"}}}}}"
 }
+
+resource "google_firestore_index" "my-index" {
+  project = "my-project-name"
+
+  collection = "chatrooms"
+
+  fields {
+    field_path = "name"
+    order      = "ASCENDING"
+  }
+
+  fields {
+    field_path = "description"
+    order      = "DESCENDING"
+  }
+
+  fields {
+    field_path = "__name__"
+    order      = "DESCENDING"
+  }
+}
