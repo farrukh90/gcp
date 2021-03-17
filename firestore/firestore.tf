e4360288d982172cb98e11eb5261f3ecf9697be9
farrukh90 <farrukhsadykov@gmail.com>
2021-03-17 04:42:28 +0000

    added json value

--- firestore/firestore.tf
+++ firestore/firestore.tf
@@ -2,5 +2,5 @@ resource "google_firestore_document" "mydoc" {
   project     = var.config["project"]
   collection  = "list"
   document_id = "users"
-  fields      = "{\"FullName\":{\"mapValue\":{\"fields\":{\"firstname\":{\"stringValue\":\"farrukh\"}}}}}"
+  fields      = file("list.json")
 }
